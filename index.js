3edbf3323b1df58ae86f3291dd0132964c768418
Bora Atik <90131638+Boreas09@users.noreply.github.com>
2023-06-29 11:45:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ console.log(merkle.toString());
 
 console.log("MerkleTree " , roothash.toString("hex"));
 console.log("Merkle proof for given wallet address " ,
-  merkle.getHexProof(keccak256("0x0469b79183bed79b00ff0c084af0da20bece16d2af2bc9f1690b1efc2db7c5b2"))
+  merkle.getHexProof(keccak256("0x0000000000000000000000000000000000000001"))
 );
 
